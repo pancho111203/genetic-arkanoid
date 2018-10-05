356085d1e07644cea75b97e0f9a22256eb831ac6
Francisco Rubin Capalbo <pancho_111203@hotmail.com>
2018-10-05 13:09:19 +0200

    s

--- src/util.js
+++ src/util.js
@@ -1,3 +1,12 @@
+Object.defineProperty(Array.prototype, 'flat', {
+  value: function(depth = 1) {
+    return this.reduce(function (flat, toFlatten) {
+      return flat.concat((Array.isArray(toFlatten) && (depth-1)) ? toFlatten.flat(depth-1) : toFlatten);
+    }, []);
+  }
+});
+
+
 export function getUrlVars() {
   if (!window || !process.browser) {
     throw new Error('Cant get url vars when not rendering');
